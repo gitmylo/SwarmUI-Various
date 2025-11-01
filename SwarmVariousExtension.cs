0b8c738fdd45d8ebace4e72b338c5b3503045449
Mylo <36931363+gitmylo@users.noreply.github.com>
2025-11-01 14:52:08 +0100

    Add main file

---
+++ SwarmVariousExtension.cs
@@ -0,0 +1,18 @@
+﻿using SwarmUI.Core;
+using SwarmUI.Utils;
+
+namespace SwarmUI.Extensions.SwarmUI_Various;
+
+public class SwarmVariousExtension : Extension
+{
+    public override void OnInit()
+    {
+        
+    }
+
+    public override void PopulateMetadata()
+    {
+        base.PopulateMetadata();
+        
+    }
+}
